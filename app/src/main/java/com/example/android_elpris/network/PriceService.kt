ca60e3946656be54236c43181f26b906dc8baf10
dehypnotic <jegrmeg@outlook.com>
2025-10-16 21:55:03 +0200

    Endringer fjernet

--- app/src/main/java/com/example/android_elpris/network/PriceService.kt
+++ app/src/main/java/com/example/android_elpris/network/PriceService.kt
@@ -3,7 +3,6 @@ package com.example.android_elpris.network
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.time.LocalDate
@@ -16,16 +15,6 @@ data class PricePoint(
     val time_end: String
 )
 
-private val certificatePinner = CertificatePinner.Builder()
-    .add("www.hvakosterstrommen.no", "sha256/jQJTbIh0grw0/1SZFhpwe7hbYWiXup2jeNjeFq85zNI=")
-    .add("www.hvakosterstrommen.no", "sha256/pL1+qb9HTMRZ/W7NdeCr2lciJA5sX2X+cZuT7vBffNo=")
-    .build()
-
-private val client = OkHttpClient.Builder()
-    .certificatePinner(certificatePinner)
-    .build()
-
-
 suspend fun fetchPrices(date: LocalDate, zone: String): List<PricePoint> {
     val url = "https://www.hvakosterstrommen.no/api/v1/prices/%d/%02d-%02d_%s.json".format(
         date.year,
@@ -34,6 +23,7 @@ suspend fun fetchPrices(date: LocalDate, zone: String): List<PricePoint> {
         zone
     )
 
+    val client = OkHttpClient()
     val req = Request.Builder().url(url).build()
     client.newCall(req).execute().use { resp ->
         if (!resp.isSuccessful) error("HTTP ${resp.code}")
