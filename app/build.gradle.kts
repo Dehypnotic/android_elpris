ca7b438f5843383ca82de9981a6948aaec60bd6f
dehypnotic <jegrmeg@outlook.com>
2026-02-09 17:36:23 +0000

    Oppdatert versjonnummer.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.dehypnotic.elpris_norge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
