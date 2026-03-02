6127b6969f538b88b04328cb9c4f217146a93c41
dehypnotic <jegrmeg@outlook.com>
2026-03-02 23:30:57 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.dehypnotic.elpris_danmark"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
